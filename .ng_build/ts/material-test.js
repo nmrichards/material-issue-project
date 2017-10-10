0a686708682cab5d80fd61b07f8cf4a945381823
Nick Richards <nick.richards@mtna.us>
2017-10-10 07:58:10 -0400

    Material beta.12 issue with ng-packagr

---
+++ .ng_build/ts/material-test.js
@@ -0,0 +1,6 @@
+/**
+ * Generated bundle index. Do not edit.
+ */
+export { AppModule } from './public_api';
+export { AppComponent as ɵa } from './src/app/app.component';
+//# sourceMappingURL=material-test.js.map
---
+++ .ng_build/ts/public_api.js
@@ -0,0 +1,2 @@
+export { AppModule } from './src/app/app.module';
+//# sourceMappingURL=public_api.js.map
